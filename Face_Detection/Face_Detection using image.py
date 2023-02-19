53a3d39eaf4024ed06c090054b3040c0747d3672
Thilagavijayan <thilagavathiv32@gmail.com>
2023-02-19 21:37:23 +0530

    solved issues

--- Face_Detection/Face_Detection using image.py
+++ Face_Detection/Face_Detection using image.py
@@ -8,9 +8,10 @@ img = cv2.imread('C:/Users/Thilaga/Documents/ML_SupervisedLearning/Face_Detectio
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+print(faces)
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 10)
 # Display the output
 cv2.imshow('img', img)
 cv2.waitKey()
